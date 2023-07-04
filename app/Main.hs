70abefe239114dace78120d25a8e02dc033514bf
Andrey_Shleger <Andrey_Shleger@epam.com>
2023-07-04 23:25:31 +0800

    func approach

--- app/Main.hs
+++ app/Main.hs
@@ -19,17 +19,29 @@ data Person = Person
 getPersonCity :: Person -> Text
 getPersonCity = addressCity . personAddress
 
+-- setPersonCity :: Text -> Person -> Person
+-- setPersonCity city person =
+--   person
+--     { personAddress =
+--         (personAddress person)
+--           { addressCity = city
+--           }
+--     }
+
+modifyAddressCity :: (Text -> Text) -> Address -> Address
+modifyAddressCity f address = address { addressCity = f (addressCity address) }
+
+modifyPersonAddress :: (Address -> Address) -> Person -> Person
+modifyPersonAddress f person = person { personAddress = f (personAddress person)}
+
+modifyPersonCity :: (Text -> Text) -> Person -> Person
+modifyPersonCity = modifyPersonAddress . modifyAddressCity
+
 setPersonCity :: Text -> Person -> Person
-setPersonCity city person =
-  person
-    { personAddress =
-        (personAddress person)
-          { addressCity = city
-          }
-    }
+setPersonCity city = modifyPersonCity (const city)
 
 main :: IO ()
 main = do
   let alice = Person {personName = "name", personAddress = Address {addressCity = "Moscow", addressStreet = "Arbat"}}
 
-  putStrLn ("out: " ++ show (setPersonCity "Voronezh" alice))
+  putStrLn ("out: " ++ show (setPersonCity  "Voronezh" alice ))
