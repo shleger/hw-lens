019d0e7ba0d0770c68a4ab0aac890848f0629e35
Andrey_Shleger <Andrey_Shleger@epam.com>
2023-07-04 23:39:47 +0800

    simple lens

--- app/Main.hs
+++ app/Main.hs
@@ -37,8 +37,44 @@ modifyPersonAddress f person = person { personAddress = f (personAddress person)
 modifyPersonCity :: (Text -> Text) -> Person -> Person
 modifyPersonCity = modifyPersonAddress . modifyAddressCity
 
+-- setPersonCity :: Text -> Person -> Person
+-- setPersonCity city = modifyPersonCity (const city)
+
+
+data Lens s a = Lens
+    { lensGetter :: s -> a
+    , lensModify :: (a -> a) -> s -> s
+    }
+
+composeLens :: Lens a b -> Lens b c -> Lens a c
+composeLens (Lens getter1 modify1) (Lens getter2 modify2) = Lens
+    { lensGetter = getter2 . getter1
+    , lensModify = modify1 . modify2
+    }
+
+
+personAddressL :: Lens Person Address
+personAddressL = Lens
+    { lensGetter = personAddress
+    , lensModify = \f person -> person { personAddress = f (personAddress person) }
+    }
+
+addressCityL :: Lens Address Text
+addressCityL = Lens
+    { lensGetter = addressCity
+    , lensModify = \f address -> address { addressCity = f (addressCity address) }
+    }
+
+personCityL :: Lens Person Text
+personCityL = personAddressL `composeLens` addressCityL
+
 setPersonCity :: Text -> Person -> Person
-setPersonCity city = modifyPersonCity (const city)
+setPersonCity city = lensModify personCityL (const city)    
+
+
+
+
+-- ============================
 
 main :: IO ()
 main = do
